5fdf56f4c817f52dcd3957c555565d39b74f039f
imon313 <imon26362@gmail.com>
2021-08-10 23:47:04 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,56 @@
 // jQuery functions
-$(document).ready(function () {
+$(document).ready(function () {});
+
+// Click Menu Events
+const buttonOpen = document.querySelector(".bar"),
+  buttonClose = document.querySelector(".close"),
+  navLink = document.querySelectorAll(".nav-link"),
+  ul = document.querySelector(".navber-nav");
+function open() {
+  const ul = document.querySelector(".navber-nav");
+  ul.classList.add("block");
+  buttonOpen.classList.add("hide");
+  buttonClose.classList.add("show");
+}
+
+function close() {
+  const ul = document.querySelector(".navber-nav");
+  ul.classList.remove("block");
+  buttonClose.classList.remove("show");
+  buttonOpen.classList.remove("hide");
+}
+
+navLink.forEach((currentLink) => {
+  currentLink.addEventListener("click", () => {
+    close();
+    ul.querySelector(".active").classList.remove("active");
+    currentLink.classList.add("active");
+  });
+});
+buttonOpen.addEventListener("click", open);
+buttonClose.addEventListener("click", close);
+// Click Event End
+
+// Scroll Event Start
+function scroll() {
+  const header = document.querySelector(".header");
+  const fixed = window.pageYOffset;
+  if (fixed > 0) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+}
+window.addEventListener("scroll", scroll);
+// Scroll Event End
+
+// Preload Start
+// function preload() {}
+
+window.addEventListener("load", () => {
+  const preload = document.querySelector(".loding");
+  preload.style.display = "none";
+
   $(".textIllate").textillate({
     // selector: ".texts",
     loop: false,
@@ -55,55 +106,4 @@ $(document).ready(function () {
     ],
   });
 });
-
-// Click Menu Events
-const buttonOpen = document.querySelector(".bar"),
-  buttonClose = document.querySelector(".close"),
-  navLink = document.querySelectorAll(".nav-link"),
-  ul = document.querySelector(".navber-nav");
-function open() {
-  const ul = document.querySelector(".navber-nav");
-  ul.classList.add("block");
-  buttonOpen.classList.add("hide");
-  buttonClose.classList.add("show");
-}
-
-function close() {
-  const ul = document.querySelector(".navber-nav");
-  ul.classList.remove("block");
-  buttonClose.classList.remove("show");
-  buttonOpen.classList.remove("hide");
-}
-
-navLink.forEach((currentLink) => {
-  currentLink.addEventListener("click", () => {
-    close();
-    ul.querySelector(".active").classList.remove("active");
-    currentLink.classList.add("active");
-  });
-});
-buttonOpen.addEventListener("click", open);
-buttonClose.addEventListener("click", close);
-// Click Event End
-
-// Scroll Event Start
-function scroll() {
-  const header = document.querySelector(".header");
-  const fixed = window.pageYOffset;
-  if (fixed > 0) {
-    header.classList.add("fixed");
-  } else {
-    header.classList.remove("fixed");
-  }
-}
-window.addEventListener("scroll", scroll);
-// Scroll Event End
-
-// Preload Start
-function preload() {
-  const preload = document.querySelector(".loding");
-  preload.style.display = "none";
-}
-
-window.addEventListener("load", preload);
 // Preload End
